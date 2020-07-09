ef7b46c51cc0c287cf4988a3535ce60c85e83bd6
sarvesh23 <42473906+sarvesh23@users.noreply.github.com>
2020-07-09 20:28:44 +0530

    Routing

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -20,8 +20,10 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Second Button ic Clicked", Toast.LENGTH_SHORT).show()
             val message: String = etUserMessage.text.toString()
             Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-
+            //Explicit Intent
             val intent = Intent(this, SecondActivity::class.java)
+
+            intent.putExtra("user_message", message)
             startActivity(intent)
 
 
