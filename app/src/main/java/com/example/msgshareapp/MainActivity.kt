a0bf145c962c5f0ee09bb5559c5b314c1efc88b9
sarvesh23 <42473906+sarvesh23@users.noreply.github.com>
2020-07-11 15:00:32 +0530

    Implicit Intent

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -28,5 +28,14 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+        btnSharetoOtherApp.setOnClickListener {
+            val message: String = etUserMessage.text.toString()
+            val intent = Intent()
+            intent.action = Intent.ACTION_SEND
+            intent.putExtra(Intent.EXTRA_TEXT, message)
+            intent.type = "text/plain"
+            startActivity(Intent.createChooser(intent, "Share to:"))
+        }
+
     }
 }
